2425c068bc14a4d926cfd7e6eefd6d5c5e935ea3
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-10-17 13:56:16 -0300

    feat: add httpClient

---
+++ src/app/modules/tracks/services/track.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrackService {
+  private readonly URL = environment.api;
+
+  constructor(
+    private httpClient: HttpClient
+  ) {
+    
+  }
+
+  getAllTracks$():Observable<any> {
+    return this.httpClient.get(`${this.URL}/tracks`).pipe(
+      map((dataRaw: any) => {
+        return dataRaw.data
+      })
+    )
+  }
+
+  getAllRandom$():Observable<any> {
+    return this.httpClient.get(`${this.URL}/tracks`).pipe(
+      map((dataRaw: any) => {
+        return dataRaw.data.reverse()
+      })
+    )
+  }
+}
