7af1f6cb25d0dd08111bd6ab641d1f8a3b1393f7
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-10-19 10:32:17 -0300

    feat: add play and pause

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -12,6 +12,7 @@ export class MultimediaService {
   public audio!:HTMLAudioElement
   public timeElapsed$:BehaviorSubject<string> = new BehaviorSubject('00:00')
   public timeRemaining$:BehaviorSubject<string> = new BehaviorSubject('-00:00')
+  public playerStatus$:BehaviorSubject<string> = new BehaviorSubject('paused')
 
   constructor() { 
     this.audio = new Audio()
@@ -25,10 +26,30 @@ export class MultimediaService {
 
   private listenAllEvents():void{
     this.audio.addEventListener('timeupdate', this.calculateTime, false)
+    this.audio.addEventListener('playing', this.setPlayerStatus, false)
+    this.audio.addEventListener('play', this.setPlayerStatus, false)
+    this.audio.addEventListener('pause', this.setPlayerStatus, false)
+    this.audio.addEventListener('ended', this.setPlayerStatus, false)
+  }
+
+  private setPlayerStatus = (state:any) => {
+    switch(state.type){
+      case 'play':
+        this.playerStatus$.next('play')
+        break
+      case 'playing':
+        this.playerStatus$.next('playing')
+        break
+      case 'ended':
+        this.playerStatus$.next('ended')
+        break
+      default: 
+        this.playerStatus$.next('paused')
+        break
+    }
   }
 
   private calculateTime = () =>{
-    console.log('Disparando evento');
     const {duration, currentTime} = this.audio
     this.setTimeElapsed(currentTime)
     this.setTimeRemaining(currentTime, duration)
@@ -54,8 +75,11 @@ export class MultimediaService {
   }
 
   public setAudio(track:TrackModel):void{
-    console.log('track:', track);
     this.audio.src = track.url
     this.audio.play()
+  }
+
+  public togglePlayer():void{
+    (this.audio.paused) ? this.audio.play() : this.audio.pause()
   }
 }
