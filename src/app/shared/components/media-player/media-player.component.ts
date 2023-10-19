df64a9587b9edde9a5a3c35d9fd286d0abfef233
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-10-19 13:52:13 -0300

    feat: add navigation through progress bar

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MultimediaService } from '@shared/services/multimedia.service';
 import { Subscription } from 'rxjs';
 
@@ -8,7 +8,7 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit, OnDestroy{
-
+  @ViewChild('progressBar') progressBar:ElementRef = new ElementRef('')
   listObservers$:Array<Subscription> = []
   state:string = 'paused'
 
@@ -21,7 +21,15 @@ export class MediaPlayerComponent implements OnInit, OnDestroy{
 
   ngOnDestroy(): void {
     this.listObservers$.forEach(u=> u.unsubscribe())
-    console.log('Component destroyed');
-    
+    console.log('Component destroyed');   
+  }
+
+  handlePosition(event: MouseEvent):void{
+    const elNative:HTMLElement = this.progressBar.nativeElement
+    const {clientX} = event
+    const {x, width} = elNative.getBoundingClientRect()
+    const clickX = clientX - x
+    const percentageFromX = (clickX * 100) / width
+    this.multimediaService.seekAudio(percentageFromX)  
   }
 }
