0388d1e1eeb0922d31c23118d3bcb9734d99eff8
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-10-19 10:11:47 -0300

    feat: add audio and times to media-player

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { TrackModel } from '@core/models/tracks.model';
 import { MultimediaService } from '@shared/services/multimedia.service';
 import { Subscription } from 'rxjs';
 
@@ -9,23 +8,13 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit, OnDestroy{
-  mockCover: TrackModel = {
-    cover: 'https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc',
-    album: 'Gioli & Assia',
-    name: 'BEBE (Oficial)',
-    url: 'http://localhost/track.mp3',
-    _id: 1
-  }
 
   listObservers$:Array<Subscription> = []
 
-  constructor(private multimediaService: MultimediaService){}
+  constructor(public multimediaService: MultimediaService){}
 
   ngOnInit(): void {
-    const observer1$: Subscription = this.multimediaService.callback.subscribe(
-      (response: TrackModel) => {console.log('Recibiendo canción:', response)}
-    )
-    this.listObservers$ = [observer1$]
+
   }
 
   ngOnDestroy(): void {
--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -1,10 +1,61 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MultimediaService {
-  callback: EventEmitter<any> = new EventEmitter<any>
+  callback: EventEmitter<any> = new EventEmitter<any>()
 
-  constructor() { }
+  public trackInfo$:BehaviorSubject<any> = new BehaviorSubject(undefined)
+  public audio!:HTMLAudioElement
+  public timeElapsed$:BehaviorSubject<string> = new BehaviorSubject('00:00')
+  public timeRemaining$:BehaviorSubject<string> = new BehaviorSubject('-00:00')
+
+  constructor() { 
+    this.audio = new Audio()
+    this.trackInfo$.subscribe(responseOK=>{
+      if(responseOK){
+        this.setAudio(responseOK)
+      }     
+    })
+    this.listenAllEvents()
+  }
+
+  private listenAllEvents():void{
+    this.audio.addEventListener('timeupdate', this.calculateTime, false)
+  }
+
+  private calculateTime = () =>{
+    console.log('Disparando evento');
+    const {duration, currentTime} = this.audio
+    this.setTimeElapsed(currentTime)
+    this.setTimeRemaining(currentTime, duration)
+  }
+
+  private setTimeElapsed(currentTime:number):void{
+    let seconds = Math.floor(currentTime % 60)
+    let minutes = Math.floor((currentTime / 60) % 60)
+    const displaySeconds = (seconds<10) ? `0${seconds}` : seconds
+    const displayMinutes = (minutes<10) ? `0${minutes}` : minutes
+    const dispayFormat = `${displayMinutes}:${displaySeconds}`
+    this.timeElapsed$.next(dispayFormat)
+  }
+
+  private setTimeRemaining(currentTime:number, duration:number):void{
+    let timeLeft = duration - currentTime
+    let seconds = Math.floor(timeLeft % 60)
+    let minutes = Math.floor((timeLeft / 60) % 60)
+    const displaySeconds = (seconds<10) ? `0${seconds}` : seconds
+    const displayMinutes = (minutes<10) ? `0${minutes}` : minutes
+    const dispayFormat = `-${displayMinutes}:${displaySeconds}`
+    this.timeRemaining$.next(dispayFormat)
+  }
+
+  public setAudio(track:TrackModel):void{
+    console.log('track:', track);
+    this.audio.src = track.url
+    this.audio.play()
+  }
 }
