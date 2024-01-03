73f77ec817eca3c88e3a90f51d7b1d4b21a25b24
Juliana Cotabarren <julianacotabarren@gmail.com>
2024-01-03 14:02:26 -0300

    feat: add deployed backend

--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -7,44 +7,41 @@ import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-auth-page',
   templateUrl: './auth-page.component.html',
-  styleUrls: ['./auth-page.component.css']
+  styleUrls: ['./auth-page.component.css'],
 })
-export class AuthPageComponent implements OnInit{
-  errorSession: boolean = false
-  formLogin: FormGroup = new FormGroup({})
+export class AuthPageComponent implements OnInit {
+  errorSession: boolean = false;
+  formLogin: FormGroup = new FormGroup({});
 
   constructor(
     private authService: AuthService,
     private cookie: CookieService,
     private router: Router
-  ){
-
-  }
+  ) {}
 
   ngOnInit(): void {
     this.formLogin = new FormGroup({
-      email: new FormControl('',[
-        Validators.required,
-        Validators.email
-      ]),
-      password: new FormControl('',[
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
         Validators.required,
         Validators.minLength(6),
-        Validators.maxLength(12)
-      ])
-    })
+        Validators.maxLength(12),
+      ]),
+    });
   }
 
-  sendLogin(): void{
-    const {email, password} = this.formLogin.value
-    this.authService.sendCredentials(email, password).subscribe(responseOk => {
-      const {tokenSession, data} = responseOk
-      this.cookie.set('token', tokenSession, 4, '/')
-      this.router.navigate(['/', 'tracks'])
-    }, 
-    err => {
-      this.errorSession = true
-      console.log('Ocurrió un error en el login', err);      
-    })   
+  sendLogin(): void {
+    const { email, password } = this.formLogin.value;
+    this.authService.sendCredentials(email, password).subscribe({
+      next: (responseOk: any) => {
+        const { tokenSession, data } = responseOk;
+        this.cookie.set('token', tokenSession, 4, '/');
+        this.router.navigate(['/', 'tracks']);
+      },
+      error: (err) => {
+        this.errorSession = true;
+        console.log('Ocurrió un error en el login', err);
+      },
+    });
   }
 }
